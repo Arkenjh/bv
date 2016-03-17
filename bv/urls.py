68e17a4237e8d4b35bd5bd2a845ff06cab9a13db
Aurelien Roy <roy.aurelien@gmail.com>
2016-03-17 22:03:28 +0100

    ok

--- bv/urls.py
+++ bv/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^workers/', include('workers.urls')),
-    #url(r'^stores/', include('stores.urls')),
+    url(r'^workers/', include('workers.urls')),
+    url(r'^stores/', include('stores.urls')),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^reports/', include('reports.urls')),
     url(r'^customers/', include('customers.urls')),
